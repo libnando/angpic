c2d08e22dbab991b229ff6ef5acc17be64b2dafb
Fernando Rosa <Fernando Rosa>
2016-04-24 01:58:00 -0300

    hello world

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,6 @@
+angular.module('angpic').controller('FotosController',function($scope){
+    $scope.foto = {
+        titulo:'Leão',        
+        url: 'http://misteriosdomundo.org/wp-content/uploads/2016/03/leao.jpg'    
+    };
+});
